0529a6554bc0dc215dc7b821dbcf3e02d36d3095
IgorKawalec <251166@edu.p.lodz.pl>
2026-03-12 11:43:28 +0100

    Poprawa buga z nakładaniem się figur przy wyborze testu

--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -9,14 +9,19 @@ import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
 public class SelectTestFigureOptionListener implements ActionListener {
 
 	private DriverManager driverManager;
+	private int counter;
 
-	public SelectTestFigureOptionListener(DriverManager driverManager) {
+	public SelectTestFigureOptionListener(DriverManager driverManager, int counter) {
 		this.driverManager = driverManager;
+		this.counter = counter;
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		FiguresJoe.figureScript1(driverManager.getCurrentDriver());
-		FiguresJoe.figureScript2(driverManager.getCurrentDriver());
+		switch(counter){
+			case 1: FiguresJoe.figureScript1(driverManager.getCurrentDriver()); break;
+			case 2: FiguresJoe.figureScript2(driverManager.getCurrentDriver()); break;
+		}
+
 	}
 }
