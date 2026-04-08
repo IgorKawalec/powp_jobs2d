6b5f4078b5f08c2fca9cfd1f9ebfcae941314ac9
IgorKawalec <251166@edu.p.lodz.pl>
2026-04-08 21:07:16 +0200

    Poprawa array commander w klasie ComplexCommand

--- src/main/java/edu/kis/powp/command/ComplexCommand.java
+++ src/main/java/edu/kis/powp/command/ComplexCommand.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 
 public class ComplexCommand{
 
-    private ArrayList<DriverCommand> commander;
+    private final ArrayList<DriverCommand> commander = new ArrayList<DriverCommand>();
 
     public void add(DriverCommand command){this.commander.add(command);}
 
